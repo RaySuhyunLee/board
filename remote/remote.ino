fda108437c869d749d84a5c3513847ef4544fd89
이수현 <SuhyunLee@YasuBook.local>
2016-02-13 02:00:21 +0900

    Implement remote controller part

--- remote/remote.ino
+++ remote/remote.ino
@@ -1,5 +1,28 @@
+#include <SoftwareSerial.h>
+
+/* bluetooth */
+#define RX 2
+#define TX 3
+SoftwareSerial bluetoothSerial(RX, TX);
+
+/* sensor */
+#define TRIGGER_PIN 0
+
+/* data send interval */
+unsigned long lastTime;
+#define SEND_INTERVAL 10
+
 void setup() {
+  bluetoothSerial.begin(9600);
 }
 
 void loop() {
+  unsigned long currentTime = millis();
+  if (currentTime - lastTime >= SEND_INTERVAL) {
+    lastTime = currentTime;
+    int trigger_value = analogRead(TRIGGER_PIN);
+    int output = map(trigger_value, 0, 1024); // FIXME change map value
+
+    bluetoothSerial.println(output);
+  }
 }
